80edf1c3edd34d292be6bdf7a82cb684f82db019
Erin Davidson <rosemary.erin.o@gmail.com>
2023-08-19 20:02:44 +0000

    roman numeral solution

--- exercises/JavaScriptChallenges/RomanNumerals/roman-numeral.js
+++ exercises/JavaScriptChallenges/RomanNumerals/roman-numeral.js
@@ -1,17 +1,82 @@
 class RomanNumeral {
-  static I = 1;
-  static V = 5;
-  static X = 10;
-  static L = 50;
-  static C = 100;
-  static D = 500;
-  static M = 1000;
-  
+  static NUMERAL = {
+    1 : 'I', 
+    5 : 'V', 
+    10 : 'X', 
+    50 : 'L', 
+    100 : 'C', 
+    500 : 'D', 
+    1000 : 'M', 
+  }
+
   constructor(number) {
     this.number = number;
+    this.digitMap = this.getDigitMap();
   }
-  
+
   toRoman() {
-    
+    //let digitMap = this.getDigitMap();
+    let romanNumeral = '';
+
+    // thousands
+    if (this.digitMap['1000']) {
+      romanNumeral = this.mappedDigitsToNumeral(1000, romanNumeral);
+    }
+
+    // hundreds
+    if (this.digitMap['100']) {
+      romanNumeral = this.mappedDigitsToNumeral(100, romanNumeral);
+    }
+
+    //tens
+    if (this.digitMap['10']) {
+      romanNumeral = this.mappedDigitsToNumeral(10, romanNumeral);
+    }
+
+    //ones
+    if (this.digitMap['1']) {
+      romanNumeral = this.mappedDigitsToNumeral(1, romanNumeral);
+    }
+
+    return romanNumeral;
+  }
+
+  getDigitMap() {
+    let number = this.number;
+    let digitMap = {};
+
+    for (let divisor = 10; divisor <= 10000; divisor *= 10) {
+      digitMap[divisor / 10] = (number % divisor);
+      number -= (number % divisor);
+    }
+    return digitMap;
+  }
+
+  //pass a number representing the digits place you want to convert to numeral
+  mappedDigitsToNumeral(digitsPlace, numeralString) {
+    if (digitsPlace === 1000) {
+      numeralString += RomanNumeral.NUMERAL[digitsPlace]
+        .repeat(this.digitMap[digitsPlace] / 1000);
+      return numeralString;
+    }
+
+    if ( this.digitMap[digitsPlace] === (9 * digitsPlace) ) {
+      numeralString += RomanNumeral.NUMERAL[digitsPlace] +
+                       RomanNumeral.NUMERAL[digitsPlace * 10];
+    } else if ( this.digitMap[digitsPlace] >= (5 * digitsPlace)) {
+      let diff = this.digitMap[digitsPlace] - (5 * digitsPlace);
+      numeralString += RomanNumeral.NUMERAL[digitsPlace * 5] +
+        RomanNumeral.NUMERAL[digitsPlace].repeat(diff / (1 * digitsPlace));
+    } else if (this.digitMap[digitsPlace] === (4 * digitsPlace)) {
+      numeralString += RomanNumeral.NUMERAL[digitsPlace] +
+                       RomanNumeral.NUMERAL[digitsPlace * 5];
+    } else {
+      numeralString += RomanNumeral.NUMERAL[digitsPlace]
+        .repeat(this.digitMap[digitsPlace] / (1 * digitsPlace));
+    }
+
+    return numeralString;
   }
 }
+
+module.exports = RomanNumeral;
