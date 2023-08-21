d095eeb67d02c404c448468d4af0841f60f10a4b
Erin Davidson <rosemary.erin.o@gmail.com>
2023-08-21 20:22:20 +0000

    ran eslint

--- exercises/JavaScriptChallenges/Octal/octal.js
+++ exercises/JavaScriptChallenges/Octal/octal.js
@@ -2,7 +2,7 @@
 /*
 input: string (valid octal formatted)
 output: Number (octal input to decimal)
-rules: 
+rules:
   - if input is an invalid octal return 0
     - what is an invalid octal?
       - no alphabetic characters
@@ -13,24 +13,21 @@ class Octal {
   constructor(octalString) {
     this.octalString = octalString;
   }
-  
+
   toDecimal() {
     if (this.octalString.match(/[a-zA-Z89]/)) return 0;
-    
+
     let octalArr = [...this.octalString];
-    
+
     let octal = 0;
-    for (let i = octalArr.length - 1; i >= 0; i--) {
-      let exp = ((octalArr.length - 1) - i);
-      let digit = octalArr[i];
-      
-      octal += digit * (8**exp);
+    for (let idx = octalArr.length - 1; idx >= 0; idx--) {
+      let exp = ((octalArr.length - 1) - idx);
+      let digit = octalArr[idx];
+
+      octal += digit * (8 ** exp);
     }
     return octal;
   }
 }
-
-// let test = new Octal('233');
-// console.log(test.toDecimal()); // 0
 
 module.exports = Octal;
