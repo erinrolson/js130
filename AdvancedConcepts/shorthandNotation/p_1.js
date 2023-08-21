5747819e020678082c623d91ad9fdd2e1d64d769
Erin Davidson <rosemary.erin.o@gmail.com>
2023-08-21 19:34:53 +0000

    shorthand notation practice problems

---
+++ AdvancedConcepts/shorthandNotation/p_1.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const arr = [1, 2, 3];
+let [ one, two, three] = arr;
+console.log(one);
+console.log(two);
+console.log(three);
+
+const obj = { a: 'a', b: 'b', c: 'c' };
+let { a, b, c } = obj;
+
+console.log(a);
+console.log(b);
+console.log(c);
